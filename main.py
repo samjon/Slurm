bf713e016426378043ee50b492651dc1ac667e5b
Sam <samjon@inbox.ru>
2024-07-28 06:29:47 +0300

    учебный файл

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print("first commit")
-print('Ветка test')
+class MyClass:
+    def __init__(self):
+        self.name = "Alice"
+
+    def greet(self):
+        print("Hello, " + self.name)
+
+obj = MyClass()
+print(dir(obj))  # Выводит список атрибутов и методов объекта 'obj'
+print(dir())  # Выводит список имен в текущей локальной области видимости
